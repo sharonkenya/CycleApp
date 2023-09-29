22f8623d43ab4b62c51880a03eca4baafea1fb6f
Sharon K <shapzmeloz@gmail.com>
2023-09-29 18:01:33 +0300

    Initial commit Signin and Signup pages working perfectly The landing page is yet to be worked on

---
+++ app/src/main/java/com/example/cycleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cycleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cycleapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
